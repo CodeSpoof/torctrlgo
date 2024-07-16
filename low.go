9dbead80c6e212cbab199b83d98cd4f55ad4cc2f
CodeSpoof <nao.s_l_t_e_el@protonmail.com>
2024-07-17 01:01:32 +0200

    added - USEFEATURE - TAKEOWNERSHIP - ADD_ONION - DEL_ONION - HSPOST - ONION_CLIENT_AUTH_ADD - ONION_CLIENT_AUTH_REMOVE - ONION_CLIENT_AUTH_VIEW - DROPOWNERSHIP - DROPTIMEOUTS

--- low.go
+++ low.go
@@ -1,9 +1,12 @@
 package torctrlgo
 
 import (
+	"bytes"
+	"encoding/base64"
 	"encoding/hex"
 	"errors"
 	"slices"
+	"strconv"
 	"strings"
 )
 
@@ -103,7 +106,7 @@ const (
 	CMD_HSFETCH                  Cmd = "HSFETCH"
 	CMD_ADD_ONION                Cmd = "ADD_ONION"
 	CMD_DEL_ONION                Cmd = "DEL_ONION"
-	CMD_HSPOST                   Cmd = "HSPOST"
+	CMD_HSPOST                   Cmd = "+HSPOST"
 	CMD_ONION_CLIENT_AUTH_ADD    Cmd = "ONION_CLIENT_AUTH_ADD"
 	CMD_ONION_CLIENT_AUTH_REMOVE Cmd = "ONION_CLIENT_AUTH_REMOVE"
 	CMD_ONION_CLIENT_AUTH_VIEW   Cmd = "ONION_CLIENT_AUTH_VIEW"
@@ -273,6 +276,14 @@ func (c *LowController) Quit() error {
 	return processErrorLine(rep[0])
 }
 
+func (c *LowController) UseFeature(features []string) error {
+	rep, err := c.sendPacket([]byte(string(CMD_USEFEATURE) + " " + strings.Join(features, " ") + "\r\n"))
+	if err != nil {
+		return err
+	}
+	return processErrorLine(rep[0])
+}
+
 func (c *LowController) Resolve(addr string, reverse bool) error {
 	s := string(CMD_RESOLVE)
 	if reverse {
@@ -331,6 +342,14 @@ func (c *LowController) GetProtocolInfo(versions []string) (*ProtocolInfo, error
 	return &ret, nil
 }
 
+func (c *LowController) TakeOwnership() error {
+	rep, err := c.sendPacket([]byte(string(CMD_TAKEOWNERSHIP) + "\r\n"))
+	if err != nil {
+		return err
+	}
+	return processErrorLine(rep[0])
+}
+
 func (c *LowController) AuthChallenge(chllngType string, clientNonce []byte) (serverHash []byte, serverNonce []byte, err error) {
 	rep, err := c.sendPacket([]byte(string(CMD_AUTHCHALLENGE) + " " + chllngType + " " + hex.EncodeToString(clientNonce) + "\r\n"))
 	if err != nil {
@@ -378,4 +397,204 @@ func (c *LowController) HSFetch(addressOrDescriptor string, servers []string) er
 		return err
 	}
 	return nil
+}
+
+type KeyType string
+
+const (
+	// KEYTYPE_X25519 Only for use in OnionClientAuth
+	KEYTYPE_X25519 KeyType = "x25519"
+	// KEYTYPE_NEW pseudo-keytype, only for use in LowController.AddOnion
+	KEYTYPE_NEW KeyType = "NEW"
+	// KEYTYPE_RSA1024 Only for use in LowController.AddOnion
+	KEYTYPE_RSA1024 KeyType = "RSA1024"
+	// KEYTYPE_ED25519_V3 Only for use in LowController.AddOnion
+	KEYTYPE_ED25519_V3 KeyType = "ED25519-V3"
+)
+
+const (
+	GENERATE_BEST       = "BEST"
+	GENERATE_RSA1024    = "RSA1024"
+	GENERATE_ED25519_V3 = "ED25519-V3"
+)
+
+type HSPortConfig struct {
+	VirtPort uint16
+	Target   string
+}
+
+type HSAuthConfig struct {
+	ClientName string
+	AuthBlob   string
+}
+
+type HSConfigReply struct {
+	ServiceID string
+	keyType   KeyType
+	keyBlob   string
+	auths     []HSAuthConfig
+}
+
+func (c *LowController) AddOnion(keyType KeyType, keyBlob string, flags []string, maxStreams uint16, ports []HSPortConfig, auths []HSAuthConfig) (*HSConfigReply, error) {
+	st := string(CMD_ADD_ONION) + " " + string(keyType) + ":" + keyBlob
+	if len(flags) > 0 {
+		st += " Flags=" + strings.Join(flags, " ")
+	}
+	if maxStreams > 0 {
+		st += " MaxStreams=" + strconv.Itoa(int(maxStreams))
+	}
+	if len(ports) == 0 {
+		return nil, ErrSyntaxCommandArgument(errors.New("missing port configuration"))
+	}
+	for _, portConfig := range ports {
+		st += " Port=" + strconv.Itoa(int(portConfig.VirtPort))
+		if len(portConfig.Target) > 0 {
+			st += "," + portConfig.Target
+		}
+	}
+	for _, authConfig := range auths {
+		if keyType == KEYTYPE_RSA1024 {
+			st += " ClientAuth=" + authConfig.ClientName
+			if len(authConfig.AuthBlob) > 0 {
+				st += ":" + authConfig.AuthBlob
+			}
+		} else if keyType == KEYTYPE_ED25519_V3 {
+			st += " ClientAuthV3=" + authConfig.AuthBlob
+		}
+	}
+	rep, err := c.sendPacket([]byte(st + "\r\n"))
+	if err != nil {
+		return nil, err
+	}
+	if err = processErrorLine(rep[len(rep)-1]); err != nil {
+		return nil, err
+	}
+	ret := HSConfigReply{}
+	for _, line := range rep {
+		if match := patternConfigValue.FindStringSubmatch(string(line.Line)); match != nil {
+			i := strings.IndexByte(match[1], ':')
+			switch match[1] {
+			case "ServiceID":
+				ret.ServiceID = strings.Trim(match[2], "\r\n ")
+			case "PrivateKey":
+				ret.keyType = KeyType(match[1][:i])
+				ret.keyBlob = match[1][i+1:]
+			case "ClientAuth":
+				ret.auths = append(ret.auths, HSAuthConfig{
+					ClientName: match[1][:i],
+					AuthBlob:   match[1][i+1:],
+				})
+			}
+		}
+	}
+	return &ret, nil
+}
+
+func (c *LowController) DelOnion(HSAddr string) error {
+	rep, err := c.sendPacket([]byte(string(CMD_DEL_ONION) + " " + HSAddr + "\r\n"))
+	if err != nil {
+		return err
+	}
+	return processErrorLine(rep[0])
+}
+
+func (c *LowController) HSPost(servers []string, HSAddr string, descriptor string) error {
+	st := string(CMD_HSPOST) + strings.Join(append([]string{""}, servers...), " SERVER=")
+	if len(HSAddr) > 0 {
+		st += " HSADDRESS=" + HSAddr
+	}
+	rep, err := c.sendPacket([]byte(st + "\r\n" + descriptor + "\r\n.\r\n"))
+	if err != nil {
+		return err
+	}
+	return processErrorLine(rep[0])
+}
+
+type OnionClientAuth struct {
+	HSAddr     string
+	KType      KeyType
+	KeyBlob    []byte
+	ClientName string
+	Flags      []string
+}
+
+func (c *LowController) OnionClientAuthAdd(auth OnionClientAuth) error {
+	st := string(CMD_ONION_CLIENT_AUTH_ADD) + " " + auth.HSAddr + " " + string(auth.KType) + ":" + base64.StdEncoding.EncodeToString(auth.KeyBlob)
+	if len(auth.ClientName) > 0 {
+		st += " ClientName=" + auth.ClientName
+	}
+	if len(auth.Flags) > 0 {
+		st += " Flags=" + strings.Join(auth.Flags, ",")
+	}
+	rep, err := c.sendPacket([]byte(st + "\r\n"))
+	if err != nil {
+		return err
+	}
+	return processErrorLine(rep[0])
+}
+
+func (c *LowController) OnionClientAuthRemove(HSAddr string) error {
+	rep, err := c.sendPacket([]byte(string(CMD_ONION_CLIENT_AUTH_REMOVE) + " " + HSAddr + "\r\n"))
+	if err != nil {
+		return err
+	}
+	return processErrorLine(rep[0])
+}
+
+func (c *LowController) OnionClientAuthView(HSAddr string) ([]OnionClientAuth, error) {
+	st := string(CMD_ONION_CLIENT_AUTH_VIEW)
+	if len(HSAddr) > 0 {
+		st += " " + HSAddr
+	}
+	rep, err := c.sendPacket([]byte(st + "\r\n"))
+	if err != nil {
+		return nil, err
+	}
+	err = processErrorLine(rep[0])
+	if err != nil {
+		return nil, err
+	}
+	var ret []OnionClientAuth
+	for _, line := range rep {
+		if i := bytes.IndexByte(line.Line, ' '); bytes.Equal(line.Line[:i], []byte("CLIENT")) {
+			segs := strings.Split(string(line.Line[i+1:]), " ")
+			keyX := strings.IndexByte(segs[1], ':')
+			blob, err := base64.StdEncoding.DecodeString(segs[1][keyX+1:])
+			if err != nil {
+				return nil, err
+			}
+			ca := OnionClientAuth{
+				HSAddr:  segs[0],
+				KType:   KeyType(segs[1][:keyX]),
+				KeyBlob: blob,
+			}
+			for i = 2; i < len(segs); i++ {
+				j := strings.IndexByte(segs[i], '=')
+				switch segs[i][:j] {
+				case "ClientName":
+					ca.ClientName = segs[i][j+1:]
+				case "Flags":
+					ca.Flags = strings.Split(segs[i][j+1:], ",")
+				}
+			}
+			ret = append(ret, ca)
+		}
+	}
+	return ret, nil
+}
+
+func (c *LowController) DropOwnership() error {
+	rep, err := c.sendPacket([]byte(string(CMD_DROPOWNERSHIP) + "\r\n"))
+	if err != nil {
+		return err
+	}
+	return processErrorLine(rep[0])
+}
+
+func (c *LowController) DropTimeouts() error {
+	rep, err := c.sendPacket([]byte(string(CMD_DROPTIMEOUTS) + "\r\n"))
+	if err != nil {
+		return err
+	}
+	return processErrorLine(rep[0])
 }
